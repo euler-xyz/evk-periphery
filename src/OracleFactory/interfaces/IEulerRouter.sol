24e175e54239f7c7759ad61e2a006261c415d455
kasperpawlowski <kasperpawlowski@gmail.com>
2024-06-14 11:47:27 +0100

    fix: pragmas and natspecs

--- src/OracleFactory/interfaces/IEulerRouter.sol
+++ src/OracleFactory/interfaces/IEulerRouter.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.0;
+
+pragma solidity >=0.8.0;
 
 /// @title IEulerRouter
+/// @custom:security-contact security@euler.xyz
 /// @author Euler Labs (https://www.eulerlabs.com/)
 /// @notice Oracle resolver interface.
 interface IEulerRouter {
