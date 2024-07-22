38e0dce36ec575dc5bfb15f9c566b53e3ebb9691
kasperpawlowski <kasperpawlowski@gmail.com>
2024-07-22 09:40:09 +0200

    feat: perspective test

---
+++ script/utils/PerspectiveTest.s.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import {BasePerspective} from "../../src/Perspectives/implementation/BasePerspective.sol";
+import {PerspectiveErrors} from "../../src/Perspectives/implementation/PerspectiveErrors.sol";
+
+contract PerspectiveTest is Script, PerspectiveErrors {
+    function run() public {
+        (bool success, bytes memory ret) = address(0x5Dc3deE40528ae713D46105787bd6072Edf4e807).call(
+            abi.encodeCall(BasePerspective.perspectiveVerify, (0xE0Da3a2096D540F4eb2E98EdD8fAe57D52EdA841, false))
+        );
+
+        if (success) {
+            console.log("Vault verified correctly");
+            return;
+        }
+
+        assembly {
+            ret := add(ret, 4)
+        }
+
+        (,, uint256 err) = abi.decode(ret, (address, address, uint256));
+
+        if (err & ERROR__FACTORY != 0) console.log("ERROR__FACTORY");
+        if (err & ERROR__IMPLEMENTATION != 0) console.log("ERROR__IMPLEMENTATION");
+        if (err & ERROR__UPGRADABILITY != 0) console.log("ERROR__UPGRADABILITY");
+        if (err & ERROR__SINGLETON != 0) console.log("ERROR__SINGLETON");
+        if (err & ERROR__NESTING != 0) console.log("ERROR__NESTING");
+        if (err & ERROR__ORACLE_INVALID_ROUTER != 0) console.log("ERROR__ORACLE_INVALID_ROUTER");
+        if (err & ERROR__ORACLE_GOVERNED_ROUTER != 0) console.log("ERROR__ORACLE_GOVERNED_ROUTER");
+        if (err & ERROR__ORACLE_INVALID_FALLBACK != 0) console.log("ERROR__ORACLE_INVALID_FALLBACK");
+        if (err & ERROR__ORACLE_INVALID_ROUTER_CONFIG != 0) console.log("ERROR__ORACLE_INVALID_ROUTER_CONFIG");
+        if (err & ERROR__ORACLE_INVALID_ADAPTER != 0) console.log("ERROR__ORACLE_INVALID_ADAPTER");
+        if (err & ERROR__UNIT_OF_ACCOUNT != 0) console.log("ERROR__UNIT_OF_ACCOUNT");
+        if (err & ERROR__CREATOR != 0) console.log("ERROR__CREATOR");
+        if (err & ERROR__GOVERNOR != 0) console.log("ERROR__GOVERNOR");
+        if (err & ERROR__FEE_RECEIVER != 0) console.log("ERROR__FEE_RECEIVER");
+        if (err & ERROR__INTEREST_FEE != 0) console.log("ERROR__INTEREST_FEE");
+        if (err & ERROR__INTEREST_RATE_MODEL != 0) console.log("ERROR__INTEREST_RATE_MODEL");
+        if (err & ERROR__SUPPLY_CAP != 0) console.log("ERROR__SUPPLY_CAP");
+        if (err & ERROR__BORROW_CAP != 0) console.log("ERROR__BORROW_CAP");
+        if (err & ERROR__HOOK_TARGET != 0) console.log("ERROR__HOOK_TARGET");
+        if (err & ERROR__HOOKED_OPS != 0) console.log("ERROR__HOOKED_OPS");
+        if (err & ERROR__CONFIG_FLAGS != 0) console.log("ERROR__CONFIG_FLAGS");
+        if (err & ERROR__NAME != 0) console.log("ERROR__NAME");
+        if (err & ERROR__SYMBOL != 0) console.log("ERROR__SYMBOL");
+        if (err & ERROR__LIQUIDATION_DISCOUNT != 0) console.log("ERROR__LIQUIDATION_DISCOUNT");
+        if (err & ERROR__LIQUIDATION_COOL_OFF_TIME != 0) console.log("ERROR__LIQUIDATION_COOL_OFF_TIME");
+        if (err & ERROR__LTV_COLLATERAL_CONFIG_LENGTH != 0) console.log("ERROR__LTV_COLLATERAL_CONFIG_LENGTH");
+        if (err & ERROR__LTV_COLLATERAL_CONFIG_SEPARATION != 0) console.log("ERROR__LTV_COLLATERAL_CONFIG_SEPARATION");
+        if (err & ERROR__LTV_COLLATERAL_CONFIG_BORROW != 0) console.log("ERROR__LTV_COLLATERAL_CONFIG_BORROW");
+        if (err & ERROR__LTV_COLLATERAL_CONFIG_LIQUIDATION != 0) {
+            console.log("ERROR__LTV_COLLATERAL_CONFIG_LIQUIDATION");
+        }
+        if (err & ERROR__LTV_COLLATERAL_RAMPING != 0) console.log("ERROR__LTV_COLLATERAL_RAMPING");
+        if (err & ERROR__LTV_COLLATERAL_RECOGNITION != 0) console.log("ERROR__LTV_COLLATERAL_RECOGNITION");
+    }
+}
