de99f4de2a0a3eef5b333fd5ff4f2af28710960e
kasperpawlowski <kasperpawlowski@gmail.com>
2024-07-30 14:39:41 +0200

    feat: HookTargetGuardian refactor

--- src/HookTarget/HookTargetGuardian.sol
+++ src/HookTarget/HookTargetGuardian.sol
@@ -2,42 +2,40 @@
 
 pragma solidity ^0.8.0;
 
-import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+import {AccessControl} from "openzeppelin-contracts/access/AccessControl.sol";
 import {IHookTarget} from "evk/interfaces/IHookTarget.sol";
-import {EVault} from "evk/EVault/EVault.sol";
 
 /// @title HookTargetGuardian
 /// @custom:security-contact security@euler.xyz
 /// @author Euler Labs (https://www.eulerlabs.com/)
-/// @notice A hook target that allows to pause specific selectors.
-contract HookTargetGuardian is IHookTarget, Ownable {
-    /// @notice The duration for which a selector remains paused.
+/// @notice A contract that allows to pause operations that are hooked for the vaults that have this contract installed
+/// as a hook target.
+contract HookTargetGuardian is IHookTarget, AccessControl {
+    /// @notice Role identifier for the guardian role.
+    bytes32 public constant GUARDIAN = keccak256("GUARDIAN");
+
+    /// @notice The duration for which vaults remain paused.
     uint256 public constant PAUSE_DURATION = 1 days;
 
-    /// @notice The cooldown period before a selector can be paused again.
+    /// @notice The cooldown period before vaults can be paused again.
     uint256 public constant PAUSE_COOLDOWN = 1 days;
 
-    /// @notice Error thrown when an operation is paused.
-    error HTG_OperationPaused();
+    /// @notice Error thrown when the vault is paused.
+    error HTG_VaultPaused();
 
-    /// @notice Event emitted when a selector is paused.
-    /// @param selector The selector that was paused.
-    event Paused(bytes4 indexed selector);
+    /// @notice Event emitted when the vaults are paused.
+    event Paused();
 
-    /// @notice Event emitted when a selector is unpaused.
-    /// @param selector The selector that was unpaused.
-    event Unpaused(bytes4 indexed selector);
+    /// @notice Event emitted when the vaults are unpaused.
+    event Unpaused();
 
-    /// @notice Mapping to store the pause timestamps for each selector.
-    mapping(bytes4 => uint256) internal pauseTimestamps;
+    /// @notice The timestamp of the last pause.
+    uint256 internal lastPauseTimestamp;
 
-    /// @notice Constructor to set the initial guardian (owner) of the contract.
-    /// @param guardian The address of the initial guardian.
-    constructor(address guardian) Ownable(guardian) {}
-
-    /// @notice Fallback function to revert if the called selector is paused.
-    fallback() external {
-        if (isPaused(bytes4(msg.data))) revert HTG_OperationPaused();
+    /// @notice Constructor to set the initial admin of the contract.
+    /// @param admin The address of the initial admin.
+    constructor(address admin) {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
     }
 
     /// @inheritdoc IHookTarget
@@ -45,49 +43,49 @@ contract HookTargetGuardian is IHookTarget, Ownable {
         return this.isHookTarget.selector;
     }
 
-    /// @notice Checks if a given selector is currently paused.
-    /// @param selector The selector to check.
-    /// @return True if the selector is paused, false otherwise.
-    function isPaused(bytes4 selector) public view returns (bool) {
-        return pauseTimestamps[selector] + PAUSE_DURATION >= block.timestamp;
+    /// @notice Fallback function to revert if the vault is paused.
+    fallback() external {
+        if (isPaused()) revert HTG_VaultPaused();
     }
 
-    /// @notice Pauses the given selectors.
-    /// @dev The selector can only be paused if it's pausable and if the cooldown period has passed since the last
-    /// pause.
-    /// @param selectors An array of selectors to be paused.
-    function pause(bytes4[] memory selectors) external onlyOwner {
-        for (uint256 i = 0; i < selectors.length; ++i) {
-            bytes4 selector = selectors[i];
-            if (
-                isNonPausable(selector)
-                    || pauseTimestamps[selector] + PAUSE_DURATION + PAUSE_COOLDOWN >= block.timestamp
-            ) continue;
+    /// @notice Pauses the vault operations.
+    /// @dev The vault can only be paused if it is currently not paused AND the cooldown period has passed since the
+    /// last pause.
+    function pause() external onlyRole(GUARDIAN) {
+        if (!isPausable()) return;
 
-            pauseTimestamps[selector] = block.timestamp;
-            emit Paused(selector);
-        }
+        lastPauseTimestamp = block.timestamp;
+        emit Paused();
     }
 
-    /// @notice Unpauses the given selectors.
-    /// @dev The selector can only be unpaused if the operation is currently paused.
-    /// @param selectors An array of selectors to be unpaused.
-    function unpause(bytes4[] memory selectors) external onlyOwner {
-        for (uint256 i = 0; i < selectors.length; ++i) {
-            bytes4 selector = selectors[i];
+    /// @notice Unpauses the vault operations.
+    /// @dev The vault can only be unpaused if it is currently paused AND if:
+    /// - the guardian is calling the function, OR
+    /// - the pause duration has passed since the last pause
+    function unpause() external {
+        if (!isUnpausable(hasRole(GUARDIAN, _msgSender()))) return;
 
-            if (!isPaused(selector)) continue;
-
-            pauseTimestamps[selector] = 1;
-            emit Unpaused(selector);
-        }
+        lastPauseTimestamp = 0;
+        emit Unpaused();
     }
 
-    /// @notice Checks if a given selector is non-pausable.
-    /// @param selector The selector to check.
-    /// @return True if the selector is non-pausable, false otherwise.
-    function isNonPausable(bytes4 selector) public pure returns (bool) {
-        return selector == EVault.transfer.selector || selector == EVault.liquidate.selector
-            || selector == EVault.checkVaultStatus.selector;
+    /// @notice Checks if the vault using this hook target is currently paused.
+    /// @return bool True if the vault is paused, false otherwise.
+    function isPaused() public view returns (bool) {
+        return lastPauseTimestamp + PAUSE_DURATION >= block.timestamp;
+    }
+
+    /// @notice Checks if the vault using this hook target can be paused.
+    /// @return bool True if the vault can be paused, false otherwise.
+    function isPausable() public view returns (bool) {
+        return lastPauseTimestamp == 0 && lastPauseTimestamp + PAUSE_DURATION + PAUSE_COOLDOWN < block.timestamp;
+    }
+
+    /// @notice Checks if the vault using this hook target can be unpaused.
+    /// @param guardianCalling Whether the guardian is calling the function.
+    /// @return bool True if the vault can be unpaused, false otherwise.
+    function isUnpausable(bool guardianCalling) public view returns (bool) {
+        return lastPauseTimestamp != 0
+            && (guardianCalling || (!guardianCalling && lastPauseTimestamp + PAUSE_DURATION < block.timestamp));
     }
 }
