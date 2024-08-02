3e220fda9f1d01952d88f3c938876cacf479634f
Mick de Graaf <mick@mickdegraaf.nl>
2024-08-02 11:42:12 +0200

    Deploy in factory constructor

--- src/EscrowFactory/EscrowFactory.sol
+++ src/EscrowFactory/EscrowFactory.sol
@@ -2,35 +2,26 @@
 
 pragma solidity ^0.8.24;
 
-import { GenericFactory } from "evk/GenericFactory/GenericFactory.sol";
-import { EscrowSingletonPerspective } from "../Perspectives/deployed/EscrowSingletonPerspective.sol";
-import { IEVault } from "evk/EVault/IEVault.sol";
-
+import {GenericFactory} from "evk/GenericFactory/GenericFactory.sol";
+import {EscrowSingletonPerspective} from "../Perspectives/deployed/EscrowSingletonPerspective.sol";
+import {IEVault} from "evk/EVault/IEVault.sol";
 
 contract EscrowFactory {
-    GenericFactory public immutable vaultFactory;
-    EscrowSingletonPerspective public immutable escrowPerspective;
+    constructor(address vaultFactory_, address escrowPerspective_, address _asset) {
+        GenericFactory vaultFactory = GenericFactory(vaultFactory_);
+        EscrowSingletonPerspective escrowPerspective = EscrowSingletonPerspective(escrowPerspective_);
 
-    constructor(address vaultFactory_, address escrowPerspective_) {
-        vaultFactory = GenericFactory(vaultFactory_);
-        escrowPerspective = EscrowSingletonPerspective(escrowPerspective_);
-    }
-
-
-    function deploy(address asset) external returns (address) {
-        IEVault vault = IEVault(vaultFactory.createProxy(
-            address(0),
-            false, // current perspective does not allow upgradeability.
-            abi.encodePacked(asset, address(0), address(0))
-        ));
+        IEVault vault = IEVault(
+            vaultFactory.createProxy(
+                address(0),
+                false, // current perspective does not allow upgradeability.
+                abi.encodePacked(_asset, address(0), address(0))
+            )
+        );
 
         vault.setGovernorAdmin(address(0));
         escrowPerspective.perspectiveVerify(address(vault), true);
-
-        return address(vault);
-    }
-
-    function getEscrowVault(address asset) external view returns (address) {
-        return escrowPerspective.assetLookup(asset);   
+        
+        selfdestruct(payable(address(0)));
     }
 }
