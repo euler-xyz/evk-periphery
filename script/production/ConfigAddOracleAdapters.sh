0a6a2828c02ada87daa33b0525b24a2c73d22662
kasperpawlowski <kasperpawlowski@gmail.com>
2024-08-28 17:19:42 +0100

    add --legacy option

--- script/production/ConfigAddOracleAdapters.sh
+++ script/production/ConfigAddOracleAdapters.sh
@@ -37,7 +37,7 @@ while IFS=, read -r -a columns || [ -n "$columns" ]; do
     entry=$(cast call $adapter_registry "entries(address)((uint128,uint128))" $adapter --rpc-url $DEPLOYMENT_RPC_URL)
 
     if [[ $entry == "(0, 0)" ]]; then
-        if cast send $adapter_registry "add(address,address,address)()" $adapter $base $quote --rpc-url $DEPLOYMENT_RPC_URL --private-key $DEPLOYER_KEY --gas-price $gasPrice > /dev/null; then
+        if cast send $adapter_registry "add(address,address,address)()" $adapter $base $quote --rpc-url $DEPLOYMENT_RPC_URL --private-key $DEPLOYER_KEY --legacy --gas-price $gasPrice > /dev/null; then
             echo "Successfully added adapter $adapterName ($adapter) to the registry."
         else
             echo "Failed to add adapter $adapterName ($adapter) to the registry."
