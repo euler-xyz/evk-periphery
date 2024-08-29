29a0b6d1310a0f187af45ba560cd18bea3a2d1eb
kasperpawlowski <kasperpawlowski@gmail.com>
2024-08-29 12:32:24 +0100

    rename whitelist perspective

--- test/Perspectives/CustomWhitelistPerspective.t.sol
+++ test/Perspectives/CustomWhitelistPerspective.t.sol
@@ -4,19 +4,19 @@ pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
 import {IPerspective} from "../../src/Perspectives/implementation/interfaces/IPerspective.sol";
-import {ImmutableWhitelistPerspective} from "../../src/Perspectives/deployed/ImmutableWhitelistPerspective.sol";
+import {CustomWhitelistPerspective} from "../../src/Perspectives/deployed/CustomWhitelistPerspective.sol";
 
-contract ImmutableWhitelistPerspectiveTest is Test {
-    function test_ImmutableWhitelistPerspective(uint8 size, uint256 seed) public {
+contract CustomWhitelistPerspectiveTest is Test {
+    function test_CustomWhitelistPerspective(uint8 size, uint256 seed) public {
         address[] memory whitelist = new address[](size);
 
         for (uint256 i = 0; i < size; i++) {
             whitelist[i] = address(uint160(uint256(keccak256(abi.encode(seed, i)))));
         }
 
-        ImmutableWhitelistPerspective perspective = new ImmutableWhitelistPerspective(whitelist);
+        CustomWhitelistPerspective perspective = new CustomWhitelistPerspective(whitelist);
 
-        assertEq(perspective.name(), "Immutable Whitelist Perspective");
+        assertEq(perspective.name(), "Custom Whitelist Perspective");
 
         for (uint256 i = 0; i < size; i++) {
             assertTrue(perspective.isVerified(whitelist[i]));
