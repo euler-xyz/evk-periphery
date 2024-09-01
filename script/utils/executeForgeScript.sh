8e906cbec9da0470312aba06455f7280ebe77152
kasperpawlowski <kasperpawlowski@gmail.com>
2024-09-01 15:39:59 +0100

    fix fs_permissions, add --with-gas-price setting

--- script/utils/executeForgeScript.sh
+++ script/utils/executeForgeScript.sh
@@ -5,7 +5,10 @@ source .env
 scriptPath=$1
 shouldVerify=$2
 
-if ! forge script script/$scriptPath --rpc-url "$DEPLOYMENT_RPC_URL" --broadcast --legacy --slow; then
+currentGasPrice=$(cast gas-price --rpc-url "$DEPLOYMENT_RPC_URL")
+gasPrice=$(echo "if ($currentGasPrice * 1.25 > 2000000000) ($currentGasPrice * 1.25)/1 else 2000000000" | bc)
+
+if ! forge script script/$scriptPath --rpc-url "$DEPLOYMENT_RPC_URL" --broadcast --legacy --slow --with-gas-price $gasPrice; then
     exit 1
 fi
 
