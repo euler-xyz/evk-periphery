f43afb81d9abd3907c90abc80e65ac99bc05280d
Doug Hoyte <doug@hcsw.org>
2024-09-01 10:06:14 -0400

    rollback fs_permissions, don't hide errors from executeForgeScript, kill --gas-price setting (use ETH_GAS_PRICE env var instead)

--- script/utils/executeForgeScript.sh
+++ script/utils/executeForgeScript.sh
@@ -5,10 +5,7 @@ source .env
 scriptPath=$1
 shouldVerify=$2
 
-currentGasPrice=$(cast gas-price --rpc-url "$DEPLOYMENT_RPC_URL")
-gasPrice=$(echo "if ($currentGasPrice * 1.25 > 2000000000) ($currentGasPrice * 1.25)/1 else 2000000000" | bc)
-
-if ! forge script script/$scriptPath --rpc-url "$DEPLOYMENT_RPC_URL" --broadcast --legacy --slow --gas-price $gasPrice; then
+if ! forge script script/$scriptPath --rpc-url "$DEPLOYMENT_RPC_URL" --broadcast --legacy --slow; then
     exit 1
 fi
 
