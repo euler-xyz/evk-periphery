d4e85809b89a4406cfb3437bfef34f7f96dad1c8
kasperpawlowski <kasperpawlowski@gmail.com>
2024-09-06 11:51:50 +0100

    improve the sanity perspective check

--- script/utils/PerspectiveCheck.s.sol
+++ script/utils/PerspectiveCheck.s.sol
@@ -11,19 +11,19 @@ contract PerspectiveCheck is Script {
     function run() public {
         address perspective = vm.envAddress("PERSPECTIVE_ADDRESS");
         address vault = vm.envAddress("VAULT_ADDRESS");
-        PerspectiveVerifier.verifyPerspective(perspective, vault);
+        PerspectiveVerifier.verifyPerspective(perspective, vault, 0);
     }
 }
 
 library PerspectiveVerifier {
-    function verifyPerspective(address perspective, address vault) internal {
+    function verifyPerspective(address perspective, address vault, uint256 expectedErrors) internal {
         console.log("Checking %s perspective for %s (%s)", perspective, IEVault(vault).symbol(), vault);
 
         (bool success, bytes memory result) =
             perspective.call(abi.encodeCall(BasePerspective.perspectiveVerify, (vault, false)));
 
         if (success) {
-            console.log("Vault verified correctly\n");
+            console.log("No errors detected\n");
             return;
         }
 
@@ -31,40 +31,42 @@ library PerspectiveVerifier {
             result := add(result, 4)
         }
 
-        (,, uint256 code) = abi.decode(result, (address, address, uint256));
+        (,, uint256 codes) = abi.decode(result, (address, address, uint256));
 
-        if (code & FACTORY != 0) console.log("ERROR__FACTORY");
-        if (code & IMPLEMENTATION != 0) console.log("ERROR__IMPLEMENTATION");
-        if (code & UPGRADABILITY != 0) console.log("ERROR__UPGRADABILITY");
-        if (code & SINGLETON != 0) console.log("ERROR__SINGLETON");
-        if (code & NESTING != 0) console.log("ERROR__NESTING");
-        if (code & ORACLE_INVALID_ROUTER != 0) console.log("ERROR__ORACLE_INVALID_ROUTER");
-        if (code & ORACLE_GOVERNED_ROUTER != 0) console.log("ERROR__ORACLE_GOVERNED_ROUTER");
-        if (code & ORACLE_INVALID_FALLBACK != 0) console.log("ERROR__ORACLE_INVALID_FALLBACK");
-        if (code & ORACLE_INVALID_ROUTER_CONFIG != 0) console.log("ERROR__ORACLE_INVALID_ROUTER_CONFIG");
-        if (code & ORACLE_INVALID_ADAPTER != 0) console.log("ERROR__ORACLE_INVALID_ADAPTER");
-        if (code & UNIT_OF_ACCOUNT != 0) console.log("ERROR__UNIT_OF_ACCOUNT");
-        if (code & CREATOR != 0) console.log("ERROR__CREATOR");
-        if (code & GOVERNOR != 0) console.log("ERROR__GOVERNOR");
-        if (code & FEE_RECEIVER != 0) console.log("ERROR__FEE_RECEIVER");
-        if (code & INTEREST_FEE != 0) console.log("ERROR__INTEREST_FEE");
-        if (code & INTEREST_RATE_MODEL != 0) console.log("ERROR__INTEREST_RATE_MODEL");
-        if (code & SUPPLY_CAP != 0) console.log("ERROR__SUPPLY_CAP");
-        if (code & BORROW_CAP != 0) console.log("ERROR__BORROW_CAP");
-        if (code & HOOK_TARGET != 0) console.log("ERROR__HOOK_TARGET");
-        if (code & HOOKED_OPS != 0) console.log("ERROR__HOOKED_OPS");
-        if (code & CONFIG_FLAGS != 0) console.log("ERROR__CONFIG_FLAGS");
-        if (code & NAME != 0) console.log("ERROR__NAME");
-        if (code & SYMBOL != 0) console.log("ERROR__SYMBOL");
-        if (code & LIQUIDATION_DISCOUNT != 0) console.log("ERROR__LIQUIDATION_DISCOUNT");
-        if (code & LIQUIDATION_COOL_OFF_TIME != 0) console.log("ERROR__LIQUIDATION_COOL_OFF_TIME");
-        if (code & LTV_COLLATERAL_CONFIG_LENGTH != 0) console.log("ERROR__LTV_COLLATERAL_CONFIG_LENGTH");
-        if (code & LTV_COLLATERAL_CONFIG_SEPARATION != 0) console.log("ERROR__LTV_COLLATERAL_CONFIG_SEPARATION");
-        if (code & LTV_COLLATERAL_CONFIG_BORROW != 0) console.log("ERROR__LTV_COLLATERAL_CONFIG_BORROW");
-        if (code & LTV_COLLATERAL_CONFIG_LIQUIDATION != 0) console.log("ERROR__LTV_COLLATERAL_CONFIG_LIQUIDATION");
-        if (code & LTV_COLLATERAL_RAMPING != 0) console.log("ERROR__LTV_COLLATERAL_RAMPING");
-        if (code & LTV_COLLATERAL_RECOGNITION != 0) console.log("ERROR__LTV_COLLATERAL_RECOGNITION");
-        console.log("Perspective check failed\n");
+        if (codes & FACTORY != 0) console.log("ERROR__FACTORY");
+        if (codes & IMPLEMENTATION != 0) console.log("ERROR__IMPLEMENTATION");
+        if (codes & UPGRADABILITY != 0) console.log("ERROR__UPGRADABILITY");
+        if (codes & SINGLETON != 0) console.log("ERROR__SINGLETON");
+        if (codes & NESTING != 0) console.log("ERROR__NESTING");
+        if (codes & ORACLE_INVALID_ROUTER != 0) console.log("ERROR__ORACLE_INVALID_ROUTER");
+        if (codes & ORACLE_GOVERNED_ROUTER != 0) console.log("ERROR__ORACLE_GOVERNED_ROUTER");
+        if (codes & ORACLE_INVALID_FALLBACK != 0) console.log("ERROR__ORACLE_INVALID_FALLBACK");
+        if (codes & ORACLE_INVALID_ROUTER_CONFIG != 0) console.log("ERROR__ORACLE_INVALID_ROUTER_CONFIG");
+        if (codes & ORACLE_INVALID_ADAPTER != 0) console.log("ERROR__ORACLE_INVALID_ADAPTER");
+        if (codes & UNIT_OF_ACCOUNT != 0) console.log("ERROR__UNIT_OF_ACCOUNT");
+        if (codes & CREATOR != 0) console.log("ERROR__CREATOR");
+        if (codes & GOVERNOR != 0) console.log("ERROR__GOVERNOR");
+        if (codes & FEE_RECEIVER != 0) console.log("ERROR__FEE_RECEIVER");
+        if (codes & INTEREST_FEE != 0) console.log("ERROR__INTEREST_FEE");
+        if (codes & INTEREST_RATE_MODEL != 0) console.log("ERROR__INTEREST_RATE_MODEL");
+        if (codes & SUPPLY_CAP != 0) console.log("ERROR__SUPPLY_CAP");
+        if (codes & BORROW_CAP != 0) console.log("ERROR__BORROW_CAP");
+        if (codes & HOOK_TARGET != 0) console.log("ERROR__HOOK_TARGET");
+        if (codes & HOOKED_OPS != 0) console.log("ERROR__HOOKED_OPS");
+        if (codes & CONFIG_FLAGS != 0) console.log("ERROR__CONFIG_FLAGS");
+        if (codes & NAME != 0) console.log("ERROR__NAME");
+        if (codes & SYMBOL != 0) console.log("ERROR__SYMBOL");
+        if (codes & LIQUIDATION_DISCOUNT != 0) console.log("ERROR__LIQUIDATION_DISCOUNT");
+        if (codes & LIQUIDATION_COOL_OFF_TIME != 0) console.log("ERROR__LIQUIDATION_COOL_OFF_TIME");
+        if (codes & LTV_COLLATERAL_CONFIG_LENGTH != 0) console.log("ERROR__LTV_COLLATERAL_CONFIG_LENGTH");
+        if (codes & LTV_COLLATERAL_CONFIG_SEPARATION != 0) console.log("ERROR__LTV_COLLATERAL_CONFIG_SEPARATION");
+        if (codes & LTV_COLLATERAL_CONFIG_BORROW != 0) console.log("ERROR__LTV_COLLATERAL_CONFIG_BORROW");
+        if (codes & LTV_COLLATERAL_CONFIG_LIQUIDATION != 0) console.log("ERROR__LTV_COLLATERAL_CONFIG_LIQUIDATION");
+        if (codes & LTV_COLLATERAL_RAMPING != 0) console.log("ERROR__LTV_COLLATERAL_RAMPING");
+        if (codes & LTV_COLLATERAL_RECOGNITION != 0) console.log("ERROR__LTV_COLLATERAL_RECOGNITION");
+
+        if (expectedErrors != codes) revert("Perspective check failed");
+        console.log("Only expected errors detected\n");
     }
 
     uint256 internal constant FACTORY = 1 << 0;
