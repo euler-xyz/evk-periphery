8826dd4b423222bc597d8279977b12c7789e22dc
kasperpawlowski <kasperpawlowski@gmail.com>
2024-09-10 13:59:44 +0100

    add verbose revert message

--- script/production/mainnet/OwnershipTransferCore.s.sol
+++ script/production/mainnet/OwnershipTransferCore.s.sol
@@ -14,9 +14,17 @@ contract OwnershipTransferCore is ScriptUtils {
     address internal constant PROTOCOL_CONFIG_ADMIN = EULER_DAO;
     address internal constant EVAULT_FACTORY_GOVERNOR_ADMIN = EULER_DAO;
 
+    function run() public {
+        startBroadcast();
+        transferOwnership();
+        stopBroadcast();
+    }
+
     function transferOwnership() internal {
         // if called by admin, the script will remove itself from default admin role in factory governor
-        require(getDeployer() != EVAULT_FACTORY_GOVERNOR_ADMIN); 
+        require(
+            getDeployer() != EVAULT_FACTORY_GOVERNOR_ADMIN, "OwnershipTransferCore: cannot be called by current admin"
+        );
 
         ProtocolConfig(coreAddresses.protocolConfig).setAdmin(PROTOCOL_CONFIG_ADMIN);
         FactoryGovernor(coreAddresses.eVaultFactoryGovernor).grantRole(
@@ -29,11 +37,5 @@ contract OwnershipTransferCore is ScriptUtils {
             FactoryGovernor(coreAddresses.eVaultFactoryGovernor).DEFAULT_ADMIN_ROLE(), getDeployer()
         );
         GenericFactory(coreAddresses.eVaultFactory).setUpgradeAdmin(coreAddresses.eVaultFactoryGovernor);
-    }
-
-    function run() public {
-        startBroadcast();
-        transferOwnership();
-        stopBroadcast();
     }
 }
