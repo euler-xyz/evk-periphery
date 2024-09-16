c53ab15093b9d19fbf718304350d9fe2181abe55
dglowinski <dariushg32111@gmail.com>
2024-09-16 18:14:49 +0200

    update tests

--- test/Governor/FactoryGovernor.scripts.t.sol
+++ test/Governor/FactoryGovernor.scripts.t.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.23;
 import {Test, console, stdError} from "forge-std/Test.sol";
-import {OwnershipTransferCore} from "../../script/production/mainnet/OwnershipTransferCore.s.sol";
 import {GenericFactory} from "evk/GenericFactory/GenericFactory.sol";
 import {FactoryGovernor} from "../../src/Governor/FactoryGovernor.sol";
 import {ProtocolConfig} from "evk/ProtocolConfig/ProtocolConfig.sol";
@@ -10,34 +9,28 @@ import {EVault} from "evk/EVault/EVault.sol";
 
 
 /// @notice The tests operate on a fork. Create a .env file with FORK_RPC_URL as per foundry docs
-contract FactoryGovernorScriptsTest is Test, OwnershipTransferCore {
-    uint256 constant BLOCK_NUMBER = 20714777;
+contract FactoryGovernorScriptsTest is Test {
+    uint256 constant BLOCK_NUMBER = 20764243;
+    address internal constant FACTORY_GOVERNOR = 0x799E9b58895d7D10306cA6C4cAb51728B142a224;
 
     address internal constant EVAULT_FACTORY = 0x29a56a1b8214D9Cf7c5561811750D5cBDb45CC8e;
-    address internal constant PROTOCOL_CONFIG = 0x4cD6BF1D183264c02Be7748Cb5cd3A47d013351b;
+    address internal constant EULER_DAO = 0xcAD001c30E96765aC90307669d578219D4fb1DCe;
     address internal EUSDC2 = 0x797DD80692c3b2dAdabCe8e30C07fDE5307D48a9;
 
-    address internal constant DEPLOYER = 0xEe009FAF00CF54C1B4387829aF7A8Dc5f0c8C8C5;
-
-    // SET AFTER DEPLOYMENT
-    address internal constant FACTORY_GOVERNOR = 0x799E9b58895d7D10306cA6C4cAb51728B142a224;
+    address internal constant EVAULT_FACTORY_GOVERNOR_ADMIN = EULER_DAO;
 
     uint256 mainnetFork;
 
     FactoryGovernor factoryGovernor;
     GenericFactory factory;
-    ProtocolConfig protocolConfig;
     EVault eUSDC;
 
     string FORK_RPC_URL = vm.envOr("FORK_RPC_URL", string(""));
 
-    constructor () OwnershipTransferCore() {
-        coreAddresses.eVaultFactory = EVAULT_FACTORY;
-        coreAddresses.protocolConfig = PROTOCOL_CONFIG;
-        coreAddresses.eVaultFactoryGovernor = FACTORY_GOVERNOR;
-    }
-    function getDeployer() internal pure override returns (address) {
-        return DEPLOYER;
+    constructor () {
+        factoryGovernor = FactoryGovernor(FACTORY_GOVERNOR);
+        factory = GenericFactory(EVAULT_FACTORY);
+        eUSDC = EVault(EUSDC2);
     }
 
     function setUp() public virtual {
@@ -46,24 +39,10 @@ contract FactoryGovernorScriptsTest is Test, OwnershipTransferCore {
         mainnetFork = vm.createSelectFork(FORK_RPC_URL);
         vm.rollFork(BLOCK_NUMBER);
 
-        // if (coreAddresses.eVaultFactoryGovernor == address(0))
-        //     coreAddresses.eVaultFactoryGovernor = address(new FactoryGovernor(getDeployer()));
 
-        factoryGovernor = FactoryGovernor(coreAddresses.eVaultFactoryGovernor);
-        factory = GenericFactory(coreAddresses.eVaultFactory);
-        protocolConfig = ProtocolConfig(coreAddresses.protocolConfig);
-        eUSDC = EVault(EUSDC2);
-
-        // temporarily set upgrade admin on factory and protocol config back to euler deployer
-        // factory.setUpgradeAdmin(DEPLOYER);
-        // protocolConfig.setAdmin(DEPLOYER);
-
-
-        // startHoax(DEPLOYER);
+        // install factory gov
         startHoax(EULER_DAO);
-        GenericFactory(coreAddresses.eVaultFactory).setUpgradeAdmin(coreAddresses.eVaultFactoryGovernor);
-
-        // transferOwnership();
+        factory.setUpgradeAdmin(FACTORY_GOVERNOR);
         vm.stopPrank();
     }
 
@@ -72,24 +51,12 @@ contract FactoryGovernorScriptsTest is Test, OwnershipTransferCore {
         return false;
     }
 
-    function test_OwnershipTransferScript_newProtocolConfigAdmin() external {
-        vm.skip(shouldSkip());
-
-        assertEq(protocolConfig.admin(), PROTOCOL_CONFIG_ADMIN);
-
-        address feeReceiver = makeAddr("newFeeReceiver");
-        vm.prank(PROTOCOL_CONFIG_ADMIN);
-        protocolConfig.setFeeReceiver(feeReceiver);
-
-        assertEq(protocolConfig.feeReceiver(), feeReceiver);
-    }
-
-    function test_OwnershipTransferScript_factoryGovernorInstalled() external {
+    function test_InstallFactoryGovernor_factoryGovernorInstalled() external {
         vm.skip(shouldSkip());
         assertEq(factory.upgradeAdmin(), address(factoryGovernor));
     }
 
-    function test_OwnershipTransferScript_factoryGovernorEnumRoles() external {
+    function test_InstallFactoryGovernor_factoryGovernorEnumRoles() external {
         vm.skip(shouldSkip());
 
         assertEq(factoryGovernor.getRoleMemberCount(factoryGovernor.DEFAULT_ADMIN_ROLE()), 1);
@@ -99,7 +66,7 @@ contract FactoryGovernorScriptsTest is Test, OwnershipTransferCore {
         assertEq(factoryGovernor.getRoleMember(factoryGovernor.GUARDIAN_ROLE(), 0), EULER_DAO);
     }
 
-    function test_OwnershipTransferScript_factoryGovernorCanUpgradeVault() external {
+    function test_InstallFactoryGovernor_factoryGovernorCanUpgradeVault() external {
         vm.skip(shouldSkip());
 
         address newImplementation = makeAddr("newImplementation");
@@ -114,7 +81,7 @@ contract FactoryGovernorScriptsTest is Test, OwnershipTransferCore {
         assertEq(factory.implementation(), newImplementation);
     }
 
-    function test_OwnershipTransferScript_factoryGovernorCanTransferAdmin() external {
+    function test_InstallFactoryGovernor_factoryGovernorCanTransferAdmin() external {
         vm.skip(shouldSkip());
 
         address newFactoryAdmin = makeAddr("newFactoryAdmin");
@@ -127,7 +94,7 @@ contract FactoryGovernorScriptsTest is Test, OwnershipTransferCore {
         assertEq(factory.upgradeAdmin(), newFactoryAdmin);
     }
 
-    function test_OwnershipTransferScript_factoryGovernorCanPauseAndUnpause() external {
+    function test_InstallFactoryGovernor_factoryGovernorCanPauseAndUnpause() external {
         vm.skip(shouldSkip());
 
         address oldImplementation = factory.implementation();
@@ -147,7 +114,7 @@ contract FactoryGovernorScriptsTest is Test, OwnershipTransferCore {
         eUSDC.touch();
     }
 
-    function test_OwnershipTransferScript_factoryGovernorGuardianCanPause() external {
+    function test_InstallFactoryGovernor_factoryGovernorGuardianCanPause() external {
         vm.skip(shouldSkip());
 
         address guardian = makeAddr("guardian");
