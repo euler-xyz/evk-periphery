6f33623e3fe4c1b803d41c502a8278d24606ad44
kasperpawlowski <kasperpawlowski@gmail.com>
2025-01-26 11:29:24 -0500

    feat: usual cluster [WIP]

---
+++ script/production/mainnet/clusters/UsualCluster.s.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity ^0.8.0;
+
+import {ManageCluster} from "./ManageCluster.s.sol";
+import {OracleVerifier} from "../../../utils/SanityCheckOracle.s.sol";
+import {ClusterDump} from "../../../utils/ClusterDump.s.sol";
+import "evk/EVault/shared/Constants.sol";
+
+contract Cluster is ManageCluster {
+    function defineCluster() internal override {
+        // define the path to the cluster addresses file here
+        cluster.clusterAddressesPath = "/script/production/mainnet/clusters/UsualCluster.json";
+
+        // do not change the order of the assets in the .assets array. if done, it must be reflected in other the other arrays the ltvs matrix.
+        // if more than one vauls has to be deployed for the same asset, it can be added in the array as many times as needed.
+        // note however, that mappings may need reworking as they always use asset address as key.
+        cluster.assets = [
+            USD0PlusPlus,
+            USD0
+        ];
+    }
+
+    function configureCluster() internal override {
+
+        // define the governors here
+        cluster.oracleRoutersGovernor = getDeployer();
+        cluster.vaultsGovernor = getDeployer();
+
+        // define unit of account here
+        cluster.unitOfAccount = USD;
+
+        // define fee receiver here and interest fee here. if needed to be defined per asset, populate the feeReceiverOverride and interestFeeOverride mappings
+        cluster.feeReceiver = address(0);
+        cluster.interestFee = 0.1e4;
+
+        //cluster.interestFeeOverride[USD0] = 0.05e4;
+
+        // define max liquidation discount here. if needed to be defined per asset, populate the maxLiquidationDiscountOverride mapping
+        cluster.maxLiquidationDiscount = 0;
+
+        // define liquidation cool off time here. if needed to be defined per asset, populate the liquidationCoolOffTimeOverride mapping
+        cluster.liquidationCoolOffTime = 1;
+
+        // define hook target and hooked ops here. if needed to be defined per asset, populate the hookTargetOverride and hookedOpsOverride mappings
+        cluster.hookTarget = address(0); // TODO
+        cluster.hookedOps = OP_DEPOSIT | OP_MINT | OP_SKIM | OP_LIQUIDATE | OP_FLASHLOAN | OP_VAULT_STATUS_CHECK;
+
+        // define config flags here. if needed to be defined per asset, populate the configFlagsOverride mapping
+        cluster.configFlags = 1;
+
+        // define oracle providers here. 
+        cluster.oracleProviders[USD0PlusPlus] = "FixedRateOracle";
+        cluster.oracleProviders[USD0        ] = "FixedRateOracle";
+
+        // define supply caps here. 0 means no supply can occur, type(uint256).max means no cap defined hence max amount
+        cluster.supplyCaps[USD0PlusPlus] = type(uint256).max;
+        cluster.supplyCaps[USD0        ] = type(uint256).max;
+
+        // define borrow caps here. 0 means no borrow can occur, type(uint256).max means no cap defined hence max amount
+        cluster.borrowCaps[USD0PlusPlus] = type(uint256).max;
+        cluster.borrowCaps[USD0        ] = type(uint256).max;
+
+        // define IRM classes here and assign them to the assets
+        {
+            // Base=5% APY,  Kink(100%)=5% APY  Max=5% APY
+            uint256[4] memory irm_fixed_rate = [uint256(1546098748700445000), uint256(0), uint256(0), uint256(type(uint32).max)];
+
+            cluster.kinkIRMParams[USD0] = irm_fixed_rate;
+        }
+    
+        // define ltv values here. columns are liability vaults, rows are collateral vaults
+        cluster.ltvs = [
+        //                        0                1     
+        //                        USD0PlusPlus     USD0  
+        /* 0  USUSD0PlusPlus */ [uint16(0.00e4), 1e4 - 1],
+        /* 1  USD0           */ [uint16(0.00e4), 0.00e4]
+        ];
+
+        cluster.borrowLTVsOverride = [
+        //                        0                1     
+        //                        USD0PlusPlus     USD0  
+        /* 0  USUSD0PlusPlus */ [uint16(0.00e4), 0.83e4],
+        /* 1  USD0           */ [uint16(0.00e4), 0.00e4]
+        ];
+    }
+
+    function postOperations() internal override {
+        for (uint256 i = 0; i < cluster.vaults.length; ++i) {
+            OracleVerifier.verifyOracleConfig(lensAddresses.oracleLens, cluster.vaults[i], false);
+        }
+
+        ClusterDump dumper = new ClusterDump();
+        dumper.dumpCluster(cluster.vaults, cluster.externalVaults);
+    }
+}
