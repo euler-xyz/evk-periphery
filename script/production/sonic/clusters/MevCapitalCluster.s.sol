4eda84fb41a12c495cfd5162eccf6a639b4056a9
kasperpawlowski <kasperpawlowski@gmail.com>
2025-02-03 18:18:41 -0500

    feat: deploy sonic cluster

--- script/production/sonic/clusters/MevCapitalCluster.s.sol
+++ script/production/sonic/clusters/MevCapitalCluster.s.sol
@@ -14,9 +14,8 @@ contract Cluster is ManageCluster {
         cluster.clusterAddressesPath = "/script/production/sonic/clusters/MevCapitalCluster.json";
 
         // do not change the order of the assets in the .assets array. if done, it must be reflected in other the other
-        // arrays the ltvs matrix.
-        // if more than one vauls has to be deployed for the same asset, it can be added in the array as many times as
-        // needed.
+        // arrays the ltvs matrix. if more than one vauls has to be deployed for the same asset, it can be added in the
+        // array as many times as needed.
         // note however, that mappings may need reworking as they always use asset address as key.
         cluster.assets = [WETH, USDC, scETH, scUSD, wstkscETH, wstkscUSD, wS, stS, wOS];
     }
@@ -57,41 +56,40 @@ contract Cluster is ManageCluster {
         // they should be referred to as "RedstoneClassicOracle".
         // in case the asset is an ERC4626 vault itself (i.e. sUSDS) and is recognized as a valid external vault as per
         // External Vaults Registry, the string should be preceeded by "ExternalVault|" prefix. this is in order to
-        // resolve
-        // the asset (vault) in the oracle router.
+        // resolve the asset (vault) in the oracle router.
         // in case the adapter is not present in the Adapter Registry, the adapter address can be passed instead in form
         // of a string.
-        cluster.oracleProviders[WETH] = "";
-        cluster.oracleProviders[USDC] = "";
-        cluster.oracleProviders[scETH] = "";
-        cluster.oracleProviders[scUSD] = "";
-        cluster.oracleProviders[wstkscETH] = "ExternalVault|";
-        cluster.oracleProviders[wstkscUSD] = "ExternalVault|";
-        cluster.oracleProviders[wS] = "";
-        cluster.oracleProviders[stS] = "ExternalVault|";
-        cluster.oracleProviders[wOS] = "ExternalVault|";
+        cluster.oracleProviders[WETH     ] = "0xF9347838C10F72332c1b64080743350069233395";
+        cluster.oracleProviders[USDC     ] = "0xdC2492409Ef8A0574cf567232b8B55919505e0Ea";
+        cluster.oracleProviders[scETH    ] = "0x56A39f7907Ca26D87f8183193528d74503Ef9B11";
+        cluster.oracleProviders[scUSD    ] = "0x5ec86ad76f29a278E8F1373927Af4854be54A963";
+        cluster.oracleProviders[wstkscETH] = "0x0afddC99E980A46f1DC481E1B59e0634Dc5b27F4";
+        cluster.oracleProviders[wstkscUSD] = "0xc32F6c8423d4c90d4E29Fb62832c3DDFDEdFc12E";
+        cluster.oracleProviders[wS       ] = "0xc59486164BDFEe0843DB80d2987Ec0E1028f7D84";
+        cluster.oracleProviders[stS      ] = "0x2c1bc59F07af5D3dFA556bbaE60179B54DE27b4d";
+        cluster.oracleProviders[wOS      ] = "ExternalVault|0xf62820B7E0146cC436d99f962450c5cDDca3Db35";
 
         // define supply caps here. 0 means no supply can occur, type(uint256).max means no cap defined hence max amount
-        cluster.supplyCaps[WETH] = 1_500;
-        cluster.supplyCaps[USDC] = 5_000_000;
-        cluster.supplyCaps[scETH] = 600;
-        cluster.supplyCaps[scUSD] = 2_000_000;
+        cluster.supplyCaps[WETH     ] = 1_500;
+        cluster.supplyCaps[USDC     ] = 5_000_000;
+        cluster.supplyCaps[scETH    ] = 600;
+        cluster.supplyCaps[scUSD    ] = 2_000_000;
         cluster.supplyCaps[wstkscETH] = 0;
         cluster.supplyCaps[wstkscUSD] = 0;
-        cluster.supplyCaps[wS] = 10_000_000;
-        cluster.supplyCaps[stS] = 4_000_000;
-        cluster.supplyCaps[wOS] = 0;
+        cluster.supplyCaps[wS       ] = 10_000_000;
+        cluster.supplyCaps[stS      ] = 4_000_000;
+        cluster.supplyCaps[wOS      ] = 0;
 
         // define borrow caps here. 0 means no borrow can occur, type(uint256).max means no cap defined hence max amount
-        cluster.borrowCaps[WETH] = 1_300;
-        cluster.borrowCaps[USDC] = 4_250_000;
-        cluster.borrowCaps[scETH] = 240;
-        cluster.borrowCaps[scUSD] = 800_000;
+        cluster.borrowCaps[WETH     ] = 1_300;
+        cluster.borrowCaps[USDC     ] = 4_250_000;
+        cluster.borrowCaps[scETH    ] = 240;
+        cluster.borrowCaps[scUSD    ] = 800_000;
         cluster.borrowCaps[wstkscETH] = 0;
         cluster.borrowCaps[wstkscUSD] = 0;
-        cluster.borrowCaps[wS] = 0;
-        cluster.borrowCaps[stS] = 1_600_000;
-        cluster.borrowCaps[wOS] = 0;
+        cluster.borrowCaps[wS       ] = 0;
+        cluster.borrowCaps[stS      ] = 1_600_000;
+        cluster.borrowCaps[wOS      ] = 0;
 
         // define IRMs
         for (uint256 i = 0; i < cluster.assets.length; i++) {
@@ -106,26 +104,17 @@ contract Cluster is ManageCluster {
 
         // define ltv values here. columns are liability vaults, rows are collateral vaults
         cluster.ltvs = [
-            //                  0                1         2        3       4        5         6      7         8
-            //                  WETH             USDC     scETH    scUSD   wstkscETH wstkscUSD wS     stS      wOS
-            /* 0  WETH      */
-            [uint16(0.0e4), 0.78e4, 0.915e4, 0.78e4, 0.915e4, 0.78e4, 0.78e4, 0.78e4, 0.78e4],
-            /* 1  USDC      */
-            [uint16(0.78e4), 0.0e4, 0.78e4, 0.915e4, 0.78e4, 0.915e4, 0.78e4, 0.78e4, 0.78e4],
-            /* 2  scETH     */
-            [uint16(0.915e4), 0.78e4, 0.0e4, 0.78e4, 0.915e4, 0.78e4, 0.78e4, 0.78e4, 0.78e4],
-            /* 3  scUSDC    */
-            [uint16(0.78e4), 0.915e4, 0.78e4, 0.0e4, 0.78e4, 0.915e4, 0.78e4, 0.78e4, 0.78e4],
-            /* 4  wstkscETH */
-            [uint16(0.915e4), 0.78e4, 0.915e4, 0.78e4, 0.0e4, 0.78e4, 0.78e4, 0.78e4, 0.78e4],
-            /* 5  wstkscUSD */
-            [uint16(0.78e4), 0.915e4, 0.78e4, 0.915e4, 0.78e4, 0.0e4, 0.78e4, 0.78e4, 0.78e4],
-            /* 6  wS        */
-            [uint16(0.78e4), 0.78e4, 0.78e4, 0.78e4, 0.78e4, 0.78e4, 0.0e4, 0.915e4, 0.915e4],
-            /* 7  stS       */
-            [uint16(0.0e4), 0.0e4, 0.0e4, 0.0e4, 0.0e4, 0.0e4, 0.915e4, 0.0e4, 0.915e4],
-            /* 8  wOS       */
-            [uint16(0.0e4), 0.0e4, 0.0e4, 0.0e4, 0.0e4, 0.0e4, 0.915e4, 0.0e4, 0.0e4]
+            //                  0                1         2        3       4        5         6       7         8
+            //                  WETH             USDC     scETH    scUSD   wstkscETH wstkscUSD wS      stS      wOS
+            /* 0  WETH      */ [uint16(0.000e4), 0.780e4, 0.915e4, 0.780e4, 0.915e4, 0.780e4, 0.780e4, 0.780e4, 0.780e4],
+            /* 1  USDC      */ [uint16(0.780e4), 0.000e4, 0.780e4, 0.915e4, 0.780e4, 0.915e4, 0.780e4, 0.780e4, 0.780e4],
+            /* 2  scETH     */ [uint16(0.915e4), 0.780e4, 0.000e4, 0.780e4, 0.915e4, 0.780e4, 0.780e4, 0.780e4, 0.780e4],
+            /* 3  scUSDC    */ [uint16(0.780e4), 0.915e4, 0.780e4, 0.000e4, 0.780e4, 0.915e4, 0.780e4, 0.780e4, 0.780e4],
+            /* 4  wstkscETH */ [uint16(0.915e4), 0.780e4, 0.915e4, 0.780e4, 0.000e4, 0.780e4, 0.780e4, 0.780e4, 0.780e4],
+            /* 5  wstkscUSD */ [uint16(0.780e4), 0.915e4, 0.780e4, 0.915e4, 0.780e4, 0.000e4, 0.780e4, 0.780e4, 0.780e4],
+            /* 6  wS        */ [uint16(0.780e4), 0.780e4, 0.780e4, 0.780e4, 0.780e4, 0.780e4, 0.000e4, 0.915e4, 0.915e4],
+            /* 7  stS       */ [uint16(0.000e4), 0.000e4, 0.000e4, 0.000e4, 0.000e4, 0.000e4, 0.915e4, 0.000e4, 0.915e4],
+            /* 8  wOS       */ [uint16(0.000e4), 0.000e4, 0.000e4, 0.000e4, 0.000e4, 0.000e4, 0.915e4, 0.000e4, 0.000e4]
         ];
 
         // define external ltvs here. columns are liability vaults, rows are collateral vaults.
@@ -134,23 +123,7 @@ contract Cluster is ManageCluster {
 
     function postOperations() internal override {
         for (uint256 i = 0; i < cluster.vaults.length; ++i) {
-            perspectiveVerify(peripheryAddresses.governedPerspective, cluster.vaults[i]);
-        }
-        executeBatchPrank(Ownable(peripheryAddresses.governedPerspective).owner());
-
-        for (uint256 i = 0; i < cluster.vaults.length; ++i) {
             OracleVerifier.verifyOracleConfig(lensAddresses.oracleLens, cluster.vaults[i], false);
-
-            PerspectiveVerifier.verifyPerspective(
-                peripheryAddresses.eulerUngovernedNzxPerspective,
-                cluster.vaults[i],
-                PerspectiveVerifier.E__ORACLE_GOVERNED_ROUTER | PerspectiveVerifier.E__GOVERNOR,
-                PerspectiveVerifier.E__LTV_COLLATERAL_CONFIG_SEPARATION
-                    | PerspectiveVerifier.E__LTV_COLLATERAL_CONFIG_BORROW
-                    | PerspectiveVerifier.E__LTV_COLLATERAL_CONFIG_LIQUIDATION
-                    | PerspectiveVerifier.E__LTV_COLLATERAL_RAMPING,
-                false
-            );
         }
 
         ClusterDump dumper = new ClusterDump();
