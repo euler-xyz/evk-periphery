e600fa411f1085930c00c7e30eddf252bdfd512d
kasperpawlowski <kasperpawlowski@gmail.com>
2025-03-12 14:28:24 +0000

    feat: berachain mev cap cluster

---
+++ script/production/berachain/clusters/ManageCluster.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity ^0.8.0;
+
+import {ManageClusterBase} from "../../ManageClusterBase.s.sol";
+
+abstract contract Addresses {
+    address internal constant USD = address(840);
+    address internal constant BTC = 0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB;
+    address internal constant WETH = 0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590;
+    address internal constant WBERA = 0x6969696969696969696969696969696969696969;
+    address internal constant beraETH = 0x6fc6545d5cDE268D5C7f1e476D444F39c995120d;
+    address internal constant WBTC = 0x0555E30da8f98308EdB960aa94C0Db47230d2B9c;
+    address internal constant HONEY = 0xFCBD14DC51f0A4d49d5E53C2E0950e0bC26d0Dce;
+    address internal constant USDC = 0x549943e04f40284185054145c6E4e9568C1D3241;
+    address internal constant STONE = 0xEc901DA9c68E90798BbBb74c11406A32A70652C3;
+    address internal constant BYUSD = 0x688e72142674041f8f6Af4c808a4045cA1D6aC82;
+}
+
+abstract contract ManageCluster is ManageClusterBase, Addresses {}
