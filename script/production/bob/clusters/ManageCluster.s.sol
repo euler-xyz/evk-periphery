c0ba88cc9748349412aa8e9979602dc059a73d34
Guillaume Chanut <guillaumechanut@macbook-pro-de-guillaume.home>
2025-03-28 10:33:00 +0100

    testing

---
+++ script/production/bob/clusters/ManageCluster.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity ^0.8.0;
+
+import {ManageClusterBase} from "../../ManageClusterBase.s.sol";
+
+abstract contract Addresses {
+    address internal constant USD = address(840);
+    address internal constant WBTC = 0x03C7054BCB39f7b2e5B2c7AcB37583e32D70Cfa3;
+    address internal constant LBTC = 0x03C7054BCB39f7b2e5B2c7AcB37583e32D70Cfa3;
+    address internal constant HybridBTC = 0x9998e05030Aee3Af9AD3df35A34F5C51e1628779;
+}
+
+abstract contract ManageCluster is ManageClusterBase, Addresses {}
