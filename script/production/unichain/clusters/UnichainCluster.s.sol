bfa132b6f9f4f83ccc68a96ee314e0cc7736b09d
totomanov <totomanov@protonmail.com>
2025-06-04 13:07:21 +0200

    feat: intermitten crs

--- script/production/unichain/clusters/UnichainCluster.s.sol
+++ script/production/unichain/clusters/UnichainCluster.s.sol
@@ -71,22 +71,22 @@ contract Cluster is ManageCluster {
         cluster.oracleProviders[UNI   ] = "0x7e262cd6226328aaf4ea5c993a952e18dd633bc8";
 
         // define supply caps here. 0 means no supply can occur, type(uint256).max means no cap defined hence max amount
-        cluster.supplyCaps[USDC  ] = 45_000_000;
-        cluster.supplyCaps[USDT0 ] = 45_000_000;
+        cluster.supplyCaps[USDC  ] = 60_000_000;
+        cluster.supplyCaps[USDT0 ] = 60_000_000;
         cluster.supplyCaps[WETH  ] = 9_000;
         cluster.supplyCaps[wstETH] = 3_000;
-        cluster.supplyCaps[weETH ] = 6_750;
+        cluster.supplyCaps[weETH ] = 9_000;
         cluster.supplyCaps[rsETH ] = 2_000;
         cluster.supplyCaps[ezETH ] = 2_000;
         cluster.supplyCaps[WBTC  ] = 0;
         cluster.supplyCaps[UNI   ] = 500_000;
 
         // define borrow caps here. 0 means no borrow can occur, type(uint256).max means no cap defined hence max amount
-        cluster.borrowCaps[USDC  ] = 40_500_000;
-        cluster.borrowCaps[USDT0 ] = 40_500_000;
+        cluster.borrowCaps[USDC  ] = 54_000_000;
+        cluster.borrowCaps[USDT0 ] = 54_000_000;
         cluster.borrowCaps[WETH  ] = 8_100;
         cluster.borrowCaps[wstETH] = 750;
-        cluster.borrowCaps[weETH ] = 1_680;
+        cluster.borrowCaps[weETH ] = 2_250;
         cluster.borrowCaps[rsETH ] = 500;
         cluster.borrowCaps[ezETH ] = 500;
         cluster.borrowCaps[WBTC  ] = 0;
