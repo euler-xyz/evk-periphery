4a33416f24afbe13424858f0924717c61e486acd
totomanov <63903313+totomanov@users.noreply.github.com>
2025-06-20 11:28:02 +0300

    feat: crs 06-17 (#315)

--- script/production/unichain/clusters/UnichainCluster.s.sol
+++ script/production/unichain/clusters/UnichainCluster.s.sol
@@ -78,7 +78,7 @@ contract Cluster is ManageCluster {
         cluster.supplyCaps[sUSDC ] = 40_000_000;
         cluster.supplyCaps[WETH  ] = 20_000;
         cluster.supplyCaps[wstETH] = 3_000;
-        cluster.supplyCaps[weETH ] = 25_000;
+        cluster.supplyCaps[weETH ] = 30_000;
         cluster.supplyCaps[rsETH ] = 2_000;
         cluster.supplyCaps[ezETH ] = 2_000;
         cluster.supplyCaps[WBTC  ] = 100;
@@ -90,7 +90,7 @@ contract Cluster is ManageCluster {
         cluster.borrowCaps[sUSDC ] = 32_000_000;
         cluster.borrowCaps[WETH  ] = 18_000;
         cluster.borrowCaps[wstETH] = 750;
-        cluster.borrowCaps[weETH ] = 10_000;
+        cluster.borrowCaps[weETH ] = 12_000;
         cluster.borrowCaps[rsETH ] = 500;
         cluster.borrowCaps[ezETH ] = 500;
         cluster.borrowCaps[WBTC  ] = 90;
