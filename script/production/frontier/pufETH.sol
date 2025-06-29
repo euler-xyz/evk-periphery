9d54d858a8f8ef1fdf4e313d1d54091dea751574
kasperpawlowski <kasperpawlowski@gmail.com>
2025-06-29 22:49:52 +0100

    fix: frontier pufETH redeployment

--- script/production/frontier/pufETH.sol
+++ script/production/frontier/pufETH.sol
@@ -7,7 +7,6 @@ import {ManageCluster} from "./ManageCluster.s.sol";
 /// @dev NOTE: Deploy ETH IRM
 contract Cluster is ManageCluster {
     address internal constant pufETH = 0xD9A442856C234a39a81a089C06451EBAa4306a72;
-    address internal constant xpufETH = 0xD7D2802f6b19843ac4DfE25022771FD83b5A7464;
 
     function defineCluster() internal override {
         // define the path to the cluster addresses file here
@@ -18,7 +17,7 @@ contract Cluster is ManageCluster {
         // if more than one vauls has to be deployed for the same asset, it can be added in the array as many times as
         // needed.
         // note however, that mappings may need reworking as they always use asset address as key.
-        cluster.assets = [WETH, pufETH, xpufETH];
+        cluster.assets = [WETH, pufETH];
     }
 
     function configureCluster() internal override {
@@ -38,22 +37,19 @@ contract Cluster is ManageCluster {
         // in case the adapter is not present in the Adapter Registry, the adapter address can be passed instead in form
         // of a string.
         cluster.oracleProviders[pufETH] = "0x49e4d0aEa709F69EcB25b1F7D9Bd44c4d9c5c455";
-        cluster.oracleProviders[xpufETH] = "0xFE0Ff10BF1837843250E18C820Db06C0464DDfDB";
 
         // define IRM classes here and assign them to the assets or refer to the adaptive IRM address directly
         {
             cluster.irms[WETH   ] = IRM_ADAPTIVE_ETH;
             cluster.irms[pufETH ] = IRM_ADAPTIVE_ETH;
-            cluster.irms[xpufETH] = IRM_ADAPTIVE_ETH;
         }
 
         // define ltv values here. columns are liability vaults, rows are collateral vaults
         cluster.ltvs = [
-            //               0         1         2
-            //               WETH      pufETH    xpufETH
-            /* 0  WETH   */ [LTV_ZERO, LTV__LOW, LTV__LOW],
-            /* 1  pufETH */ [LTV__LOW, LTV_ZERO, LTV_ZERO],
-            /* 2  xpufETH*/ [LTV__LOW, LTV_HIGH, LTV_ZERO]
+            //               0         1
+            //               WETH      pufETH
+            /* 0  WETH   */ [LTV_ZERO, LTV__LOW],
+            /* 1  pufETH */ [LTV__LOW, LTV_ZERO]
         ];
     }
 }
