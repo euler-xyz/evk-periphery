91af8e8b9c37849bdc6fb70c1c200e04fe0129e5
dglowinski <dariushg32111@gmail.com>
2025-09-03 10:36:45 +0200

    add SwapperOwnable

--- src/Swaps/SwapperOwnable.sol
+++ src/Swaps/SwapperOwnable.sol
@@ -2,7 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+import {Ownable, Context} from "openzeppelin-contracts/access/Ownable.sol";
+import {EVCUtil} from "ethereum-vault-connector/utils/EVCUtil.sol";
 import {Swapper} from "./Swapper.sol";
 import {ISwapper} from "./ISwapper.sol";
 
@@ -11,34 +12,42 @@ import {ISwapper} from "./ISwapper.sol";
 /// @author Euler Labs (https://www.eulerlabs.com/)
 /// @notice Untrusted helper contract for EVK for performing swaps and swaps to repay. Ownable version.
 /// @dev Ownership is not transferrable not to violate external whitelistings
-contract SwapperOwnable is Swapper, Ownable {
-    constructor(address owner, address uniswapRouterV2, address uniswapRouterV3)
-        Ownable(owner)
+contract SwapperOwnable is Swapper, EVCUtil, Ownable {
+    error OwnershipNotTransferrable();
+
+    modifier onlyOwnerOrSelf() {
+        if (msg.sender != address(this)) _checkOwner();
+        _;
+    }
+
+    constructor(address evc, address owner, address uniswapRouterV2, address uniswapRouterV3)
         Swapper(uniswapRouterV2, uniswapRouterV3)
+        EVCUtil(evc)
+        Ownable(owner)
     {}
 
     /// @inheritdoc ISwapper
-    function swap(SwapParams memory params) public override onlyOwner {
+    function swap(SwapParams memory params) public override onlyOwnerOrSelf {
         super.swap(params);
     }
 
     /// @inheritdoc ISwapper
-    function repay(address token, address vault, uint256 repayAmount, address account) public override onlyOwner {
+    function repay(address token, address vault, uint256 repayAmount, address account) public override onlyOwnerOrSelf {
         super.repay(token, vault, repayAmount, account);
     }
 
     /// @inheritdoc ISwapper
-    function repayAndDeposit(address token, address vault, uint256 repayAmount, address account) public override onlyOwner {
+    function repayAndDeposit(address token, address vault, uint256 repayAmount, address account) public override onlyOwnerOrSelf {
         super.repayAndDeposit(token, vault, repayAmount, account);
     }
 
     /// @inheritdoc ISwapper
-    function deposit(address token, address vault, uint256 amountMin, address account) public override onlyOwner {
+    function deposit(address token, address vault, uint256 amountMin, address account) public override onlyOwnerOrSelf {
         super.deposit(token, vault, amountMin, account);
     }
 
     /// @inheritdoc ISwapper
-    function sweep(address token, uint256 amountMin, address to) public override onlyOwner {
+    function sweep(address token, uint256 amountMin, address to) public override onlyOwnerOrSelf {
         super.sweep(token, amountMin, to);
     }
 
@@ -48,6 +57,10 @@ contract SwapperOwnable is Swapper, Ownable {
     }
 
     function transferOwnership(address) public pure override {
-        revert("ownership not transferrable");
+        revert OwnershipNotTransferrable();
+    }
+
+    function _msgSender() internal view override (Context, EVCUtil) returns (address) {
+        return EVCUtil._msgSender();
     }
 }
