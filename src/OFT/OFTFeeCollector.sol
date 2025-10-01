f192107376ee156e9bf960549bb82fbf93dc0b5b
kasperpawlowski <kasperpawlowski@gmail.com>
2025-10-01 02:20:05 +0100

    chore: refactor; feat: FeeCollectorUtil & FeeCollectorGulper

--- src/OFT/OFTFeeCollector.sol
+++ src/OFT/OFTFeeCollector.sol
@@ -2,29 +2,17 @@
 
 pragma solidity ^0.8.0;
 
-import {AccessControlEnumerable} from "openzeppelin-contracts/access/extensions/AccessControlEnumerable.sol";
 import {IERC20, SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
-import {EnumerableSet} from "openzeppelin-contracts/utils/structs/EnumerableSet.sol";
 import {IOFT, SendParam} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/interfaces/IOFT.sol";
 import {MessagingFee} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OAppSender.sol";
-import {IEVault} from "evk/EVault/IEVault.sol";
+import {FeeCollectorUtil} from "../Util/FeeCollectorUtil.sol";
 
 /// @title OFTFeeCollector
 /// @custom:security-contact security@euler.xyz
 /// @author Euler Labs (https://www.eulerlabs.com/)
-/// @notice A contract that converts fees on multiple EVaults and sends them cross-chain via LayerZero OFT adapter.
-contract OFTFeeCollector is AccessControlEnumerable {
+/// @notice Collects and converts fees from multiple vaults, then sends them cross-chain via a LayerZero OFT adapter.
+contract OFTFeeCollector is FeeCollectorUtil {
     using SafeERC20 for IERC20;
-    using EnumerableSet for EnumerableSet.AddressSet;
-
-    /// @notice Role that can add and remove vaults from the list
-    bytes32 public constant MAINTAINER_ROLE = keccak256("MAINTAINER_ROLE");
-
-    /// @notice Role that can execute the fee collection process
-    bytes32 public constant COLLECTOR_ROLE = keccak256("COLLECTOR_ROLE");
-
-    /// @notice The ERC20 token used for fees (retrieved from the OFT adapter)
-    IERC20 public feeToken;
 
     /// @notice The LayerZero OFT adapter contract used for cross-chain transfers
     address public oftAdapter;
@@ -38,84 +26,39 @@ contract OFTFeeCollector is AccessControlEnumerable {
     /// @notice Whether to use composed message for cross-chain communication
     bool public isComposedMsg;
 
-    /// @notice Internal set of vault addresses from which fees are collected
-    EnumerableSet.AddressSet internal _vaultsList;
-
-    /// @notice Emitted when a vault is added to the list
-    event VaultAdded(address indexed vault);
-
-    /// @notice Emitted when a vault is removed from the list
-    event VaultRemoved(address indexed vault);
-
-    /// @notice Emitted when a vault asset is not the same as the fee token
-    error InvalidVault();
+    /// @notice Error thrown when the OFT adapter token is not the same as the fee token
+    error InvalidOFTAdapter();
 
     /// @notice Initializes the OFTFeeCollector contract
     /// @param _admin The address that will be granted the DEFAULT_ADMIN_ROLE
-    constructor(address _admin) {
-        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
-    }
+    /// @param _feeToken The address of the ERC20 token used for fees
+    constructor(address _admin, address _feeToken) FeeCollectorUtil(_admin, _feeToken) {}
 
-    /// @notice Configures the OFTFeeCollector contract
+    /// @notice Configures the OFTFeeCollector contract for cross-chain fee transfers
     /// @param _oftAdapter The LayerZero OFT adapter contract address
-    /// @param _dstAddress The destination address on the target chain
+    /// @param _dstAddress The destination address on the target chain to receive fees
     /// @param _dstEid The LayerZero endpoint ID of the destination chain
     /// @param _isComposedMsg Whether to use composed message for cross-chain communication
     function configure(address _oftAdapter, address _dstAddress, uint32 _dstEid, bool _isComposedMsg)
         public
         onlyRole(DEFAULT_ADMIN_ROLE)
     {
-        if (_oftAdapter != address(0)) feeToken = IERC20(IOFT(_oftAdapter).token());
+        if (_oftAdapter != address(0) && address(feeToken) != IOFT(_oftAdapter).token()) {
+            revert InvalidOFTAdapter();
+        }
+
         oftAdapter = _oftAdapter;
         dstAddress = _dstAddress;
         dstEid = _dstEid;
         isComposedMsg = _isComposedMsg;
     }
 
-    /// @notice Allows to recover any ERC20 tokens or native currency sent to this contract
-    /// @param token The address of the token to recover. If address(0), the native currency is recovered.
-    /// @param to The address to send the tokens to
-    /// @param amount The amount of tokens to recover
-    function recoverToken(address token, address to, uint256 amount) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        if (token == address(0)) {
-            (bool success,) = to.call{value: amount}("");
-            require(success, "Native currency recovery failed");
-        } else {
-            IERC20(token).safeTransfer(to, amount);
-        }
-    }
-
-    /// @notice Adds a vault to the list
-    /// @param vault The address of the vault to add
-    /// @return success True if the vault was successfully added, false if it was already in the list
-    function addToVaultsList(address vault) external onlyRole(MAINTAINER_ROLE) returns (bool) {
-        if (IEVault(vault).asset() != address(feeToken)) revert InvalidVault();
-
-        bool success = _vaultsList.add(vault);
-        if (success) emit VaultAdded(vault);
-        return success;
-    }
-
-    /// @notice Removes a vault from the list
-    /// @param vault The address of the vault to remove
-    /// @return success True if the vault was successfully removed, false if it was not in the list
-    function removeFromVaultsList(address vault) external onlyRole(MAINTAINER_ROLE) returns (bool) {
-        bool success = _vaultsList.remove(vault);
-        if (success) emit VaultRemoved(vault);
-        return success;
-    }
-
-    /// @notice Collects fees from all vaults in the list and sends them cross-chain
-    function collectFees() external onlyRole(COLLECTOR_ROLE) {
+    /// @notice Collects and converts fees from all vaults, then sends them cross-chain to the configured destination.
+    function collectFees() external virtual override onlyRole(COLLECTOR_ROLE) {
         address adapter = oftAdapter;
-        if (oftAdapter == address(0)) return;
+        if (adapter == address(0)) return;
 
-        uint256 length = _vaultsList.length();
-        for (uint256 i = 0; i < length; ++i) {
-            address vault = _vaultsList.at(i);
-            try IEVault(vault).convertFees() {} catch {}
-            try IEVault(vault).redeem(type(uint256).max, address(this), address(this)) {} catch {}
-        }
+        _convertAndRedeemFees();
 
         IERC20 token = feeToken;
         uint256 balance = token.balanceOf(address(this));
@@ -134,18 +77,5 @@ contract OFTFeeCollector is AccessControlEnumerable {
 
         token.forceApprove(adapter, balance);
         IOFT(adapter).send{value: fee.nativeFee}(sendParam, fee, address(this));
-    }
-
-    /// @notice Checks if a vault is in the list
-    /// @param vault The address of the vault to check
-    /// @return True if the vault is in the list, false otherwise
-    function isInVaultsList(address vault) external view returns (bool) {
-        return _vaultsList.contains(vault);
-    }
-
-    /// @notice Returns the complete list of vault addresses
-    /// @return An array containing all vault addresses in the list
-    function getVaultsList() external view returns (address[] memory) {
-        return _vaultsList.values();
     }
 }
---
+++ src/Util/FeeCollectorGulper.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity ^0.8.0;
+
+import {IERC20, SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+import {EulerSavingsRate} from "evk/Synths/EulerSavingsRate.sol";
+import {FeeCollectorUtil} from "./FeeCollectorUtil.sol";
+
+/// @title FeeCollectorGulper
+/// @custom:security-contact security@euler.xyz
+/// @author Euler Labs (https://www.eulerlabs.com/)
+/// @notice Collects and converts fees from multiple vaults, then deposits them into an EulerSavingsRate contract and
+/// calls gulp.
+contract FeeCollectorGulper is FeeCollectorUtil {
+    using SafeERC20 for IERC20;
+
+    /// @notice The EulerSavingsRate contract where collected fees are deposited and gulped.
+    EulerSavingsRate public immutable esr;
+
+    /// @notice Initializes the FeeCollectorGulper contract.
+    /// @param _admin The address to be granted the DEFAULT_ADMIN_ROLE.
+    /// @param _feeToken The address of the ERC20 token used for fees.
+    /// @param _esr The address of the EulerSavingsRate contract to receive collected fees.
+    constructor(address _admin, address _feeToken, address _esr) FeeCollectorUtil(_admin, _feeToken) {
+        esr = EulerSavingsRate(_esr);
+    }
+
+    /// @notice Collects and converts fees from all vaults in the list, then deposits them into the EulerSavingsRate
+    /// contract and calls gulp.
+    function collectFees() external virtual override onlyRole(COLLECTOR_ROLE) {
+        _convertAndRedeemFees();
+
+        uint256 balance = feeToken.balanceOf(address(this));
+        if (balance == 0) return;
+
+        feeToken.safeTransfer(address(esr), balance);
+        esr.gulp();
+    }
+}
