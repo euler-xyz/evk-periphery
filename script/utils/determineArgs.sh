ef7cf1bdba7fba1b1aaa3f1e3d0d8855acf2a505
kasperpawlowski <kasperpawlowski@gmail.com>
2025-12-02 20:37:35 +0000

    add network aliases

--- script/utils/determineArgs.sh
+++ script/utils/determineArgs.sh
@@ -38,30 +38,51 @@ if ! cast chain-id --rpc-url "$DEPLOYMENT_RPC_URL" &>/dev/null; then
     else
         chains_data=$(curl -s https://chainid.network/chains_mini.json)
 
-        if [ "$DEPLOYMENT_RPC_URL" = "mainnet" ] || [ "$DEPLOYMENT_RPC_URL" = "ethereum" ]; then
-            network_name="ethereum mainnet"
-        elif [ "$DEPLOYMENT_RPC_URL" = "optimism" ]; then
-            network_name="op mainnet"
-        elif [ "$DEPLOYMENT_RPC_URL" = "arbitrum" ]; then
-            network_name="arbitrum one"
-        elif [ "$DEPLOYMENT_RPC_URL" = "avalanche" ]; then
-            network_name="avalanche c-chain"
-        elif [ "$DEPLOYMENT_RPC_URL" = "swell" ]; then
-            network_name="swellchain"
-        else
-            network_name=$(echo "$DEPLOYMENT_RPC_URL" | tr '[:upper:]' '[:lower:]')
+        # Networks not in chainid.network - direct chain ID mapping
+        case $(echo "$DEPLOYMENT_RPC_URL" | tr '[:upper:]' '[:lower:]') in
+            hyperevm|hyper) chain_id=999 ;;
+            *) chain_id="" ;;
+        esac
+
+        if [ -z "$chain_id" ]; then
+            case $(echo "$DEPLOYMENT_RPC_URL" | tr '[:upper:]' '[:lower:]') in
+                mainnet|ethereum) network_name="ethereum mainnet" ;;
+                optimism|op) network_name="op mainnet" ;;
+                arbitrum|arb) network_name="arbitrum one" ;;
+                avalanche|avax) network_name="avalanche c-chain" ;;
+                swell) network_name="swellchain" ;;
+                polygon|matic) network_name="polygon mainnet" ;;
+                gnosis|xdai) network_name="gnosis" ;;
+                bsc|bnb) network_name="bnb smart chain mainnet" ;;
+                linea) network_name="linea" ;;
+                berachain|bera) network_name="berachain" ;;
+                mantle) network_name="mantle" ;;
+                worldchain|world) network_name="world chain" ;;
+                ink) network_name="ink" ;;
+                bob) network_name="bob" ;;
+                sonic) network_name="sonic mainnet" ;;
+                unichain|uni) network_name="unichain" ;;
+                corn) network_name="corn" ;;
+                morph) network_name="morph" ;;
+                rootstock|rsk) network_name="rootstock mainnet" ;;
+                plasma) network_name="plasma mainnet" ;;
+                tac) network_name="tac mainnet" ;;
+                monad) network_name="monad" ;;
+                *) network_name=$(echo "$DEPLOYMENT_RPC_URL" | tr '[:upper:]' '[:lower:]') ;;
+            esac
         fi
         
-        if ! [[ "$network_name" =~ ^[0-9]+$ ]]; then
-            chain_id=$(echo "$chains_data" | jq -r '
-                def words(str): str | ascii_downcase | split(" ");
-                def matches(network; search): 
-                    (words(search) - words(network)) | length == 0;
-                .[] | select(matches(.name; $search)) | .chainId
-            ' --arg search "$network_name" | head -n1)
-        else
-            chain_id=$network_name
-            network_name=$(echo "$chains_data" | jq -r ".[] | select(.chainId == $chain_id) | .name" | head -n1 | tr '[:upper:]' '[:lower:]')
+        if [ -z "$chain_id" ]; then
+            if ! [[ "$network_name" =~ ^[0-9]+$ ]]; then
+                chain_id=$(echo "$chains_data" | jq -r '
+                    def words(str): str | ascii_downcase | split(" ");
+                    def matches(network; search): 
+                        (words(search) - words(network)) | length == 0;
+                    .[] | select(matches(.name; $search)) | .chainId
+                ' --arg search "$network_name" | head -n1)
+            else
+                chain_id=$network_name
+            fi
         fi
 
         env_var="DEPLOYMENT_RPC_URL_${chain_id}"
